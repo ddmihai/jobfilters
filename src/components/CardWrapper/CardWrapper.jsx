f39b9491527e0382dbc4901cbd9f576409851da0
daniel mihai <sasdaniel9@gmail.com>
2024-02-28 13:37:56 +0000

    added latest SCSS

--- src/components/CardWrapper/CardWrapper.jsx
+++ src/components/CardWrapper/CardWrapper.jsx
@@ -9,10 +9,21 @@ const CardWrapper = ({ company }) => {
     
     const [imageSrc, setImageSrc] = useState(null);
 
+    // Map over technologies
+    const mapOvertechnologies = (array, field) => {
+        return array?.map((company, index) => <p key={index}>{ company }</p>);
+    }
+
+    const renderedTechnologies = mapOvertechnologies(company.languages, 'languages');
+    const renderedTools = mapOvertechnologies(company.tools, 'tools');
+
+
+    
     useEffect(() => {
         const loadImage = async () => {
             try {
-                const dynamicImage = await /* @vite-ignore */ import(`../../assets/images/${company.logo}`);
+                /* @vite-ignore */
+                const dynamicImage = await import(`../../assets/images/${company.logo}`);
                 setImageSrc(dynamicImage.default);
             } catch (error) {
                 console.error('Error loading image:', error);
@@ -21,6 +32,8 @@ const CardWrapper = ({ company }) => {
 
         loadImage();
     }, [company.logo]);
+
+
 
     return (
         <figure className={styles.figureJobCard}>
@@ -39,13 +52,22 @@ const CardWrapper = ({ company }) => {
                     </div>
 
                     {/* Ohters */}
+                    <div className={styles.positionDetails}>
+                        <p className={styles.companyPositionTitle}>{ company.position }</p>
+                        <ul>
+                            <li>{ company.postedAt }</li>
+                            <li>{ company.contract }</li>
+                            <li>{ company.location }</li>
+                        </ul>
+                    </div>
                 </article>
 
                 {/* Wrapper for the technologies */}
                 <article className={styles.texhnologiesWrapper}>
-                    <p>Technologies</p>
-                    <p>Technologies</p>
-                    <p>Technologies</p>
+                    { renderedTechnologies }
+                    { renderedTools }
+                    <p>{ company.role }</p>
+                    <p>{ company.level }</p>
                 </article>
             </aside>
         </figure>
