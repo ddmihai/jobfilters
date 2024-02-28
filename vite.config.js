83f9b4a3e40b5c90f884a2edc5ec13341b3e03cd
daniel mihai <sasdaniel9@gmail.com>
2024-02-28 16:06:44 +0000

    added includes asset directory

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/jobfilters/',
-  plugins: [react()]
+  
+  build: {
+    assetsInclude: ['src/assets/**']
+  },
+
+  plugins: [
+    react()
+  ]  
 })
